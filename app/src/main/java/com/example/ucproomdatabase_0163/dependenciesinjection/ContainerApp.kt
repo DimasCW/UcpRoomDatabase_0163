fd0f2d5dc982d7aaaca0790711eba0f4b9d39025
DimasCW <cdimas292@gmail.com>
2024-12-22 14:07:19 +0700

    menyesuaikan file ContainerApp.kt dan membuat file LandingPage.kt

--- app/src/main/java/com/example/ucproomdatabase_0163/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucproomdatabase_0163/dependenciesinjection/ContainerApp.kt
@@ -1,17 +1,24 @@
 package com.example.ucproomdatabase_0163.dependenciesinjection
 
 import android.content.Context
+import com.example.ucproomdatabase_0163.data.database.DsnDatabase
 import com.example.ucproomdatabase_0163.data.database.MkDatabase
+import com.example.ucproomdatabase_0163.repository.LocalRepositoryDsn
 import com.example.ucproomdatabase_0163.repository.LocalRepositoryMk
+import com.example.ucproomdatabase_0163.repository.RepositoryDsn
 import com.example.ucproomdatabase_0163.repository.RepositoryMk
 
 interface InterfaceContainerApp{
     val repositoryMk: RepositoryMk
+    val repositoryDsn: RepositoryDsn
 }
 
 class ContainerApp(private val context: Context): InterfaceContainerApp{
 
     override val repositoryMk: RepositoryMk by lazy {
         LocalRepositoryMk(MkDatabase.getDatabase(context).matakuliahDao())
+    }
+    override val repositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(DsnDatabase.getDatabase(context).dosenDao())
     }
 }
